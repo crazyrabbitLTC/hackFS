a7e2442d9fa8ba225dd9338413c5c02f2960e2a6
Dennison Bertram <dennison@dennisonbertram.com>
2020-07-23 11:17:57 -0400

    📦 NEW:  Add new React app with bootstrap

---
+++ app/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {NavBar} from './components/navBar/NavBar'
+import {Footer} from './components/footer/Footer'
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (<footer className='footer mt-auto py-3 bg-dark text-white'>
+        <div className='container'>Place sticky footer content here.</div>
+    </footer>)
+}
